9932a9d439285cbc10dca3c358ee921b96711976
Jesper Hodge <jhodge@outlook.de>
2016-06-03 12:35:02 +0200

    Create posts.php

---
+++ includes/posts.php
@@ -0,0 +1,12 @@
+<?PHP
+
+function get_categories()
+{
+	//FUNCTION CODE
+	//Function should return an array that holds an object for each category which stores the mysql data from the categories table.
+	//the objects have to have the "title" property.
+	
+	return $cat_array;
+}
+
+?>
