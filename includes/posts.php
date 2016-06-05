3929b53c1c4766b007e2103419fe00be735b4558
olgierd-on-fire <olgierd.everac@gmail.com>
2016-06-06 06:41:53 +0900

    Keep PSR-2

--- includes/posts.php
+++ includes/posts.php
@@ -1,12 +1,9 @@
-<?PHP
-
+<?php
 function get_categories()
 {
-	//FUNCTION CODE
-	//Function should return an array that holds an object for each category which stores the mysql data from the categories table.
-	//the objects have to have the "title" property.
-	
-	return $cat_array;
-}
+    //FUNCTION CODE
+    //Function should return an array that holds an object for each category which stores the mysql data from the categories table.
+    //the objects have to have the "title" property.
 
-?>
+    return $cat_array;
+}
