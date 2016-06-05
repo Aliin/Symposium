041e11ada02407826ef9c4723efb441f86c37008
Jesper Hodge <Aliin@users.noreply.github.com>
2016-06-06 04:26:53 +0800

    Update run_setup.php

--- database/run_setup.php
+++ database/run_setup.php
@@ -2,9 +2,15 @@
 
 require 'setup_functions.php';
 
-setup_db();
+$isSetup = setup_db();
 
+if ($isSetup == 0)
+{
+  echo 'database set up. Please check that everything is in order, otherwise make a bug report.';
+} else {
+  echo "database couldn't be set up. Please please please make a bug report.";
+}
 
-
+echo "you can close this window now.";
 
 ?>
