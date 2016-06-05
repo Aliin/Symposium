211ab1e3997046361522996f0815fbdd3b8fb57c
Jesper Hodge <Aliin@users.noreply.github.com>
2016-06-06 04:16:33 +0800

    Update mysql_setup.php
    A form that takes phpmyadmin user information to set up the database.

--- database/mysql_setup.php
+++ database/mysql_setup.php
@@ -1,51 +1,27 @@
-<?PHP
-
-function setup_db() {
-
-	require 'db_connect.php';
-
-	$clearTables = "IF OBJECT_ID('posts', 'U') IS NOT NULL 
-  DROP TABLE posts; IF OBJECT_ID('people', 'U') IS NOT NULL 
-  DROP TABLE people; IF OBJECT_ID('categories', 'U') IS NOT NULL 
-  DROP TABLE categories";
-  
-  mysqli_query($connection, $clearTables);
-	
-	$sql = "CREATE TABLE posts(
-	 id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	 author_id INT UNSIGNED NOT NULL,
-	 title VARCHAR(255) NOT NULL,
-	 body TEXT NOT NULL,
-	 category_id INT NOT NULL,
-	 reg_date TIMESTAMP
-	 )";
-	 
-	mysqli_query($connection, $sql);
-	 
-	$sql = "CREATE TABLE people (
-	 id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	 username VARCHAR(15) NOT NULL,
-	 first_name VARCHAR(15) NOT NULL,
-	 last_name VARCHAR(15) NOT NULL,
-	 email VARCHAR(50) NOT NULL,
-	 url VARCHAR(155)
-	 )";
-	 
-	mysqli_query($connection, $sql);
-
-	$sql = "CREATE TABLE categories (
-	  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	  name VARCHAR(20) NOT NULL
-	  )";
-	  
-	mysqli_close($connection);
-}
-
-?>
 <!DOCTYPE html>
-<html>
-<head><title>Setup Database</title></head>
-<body>
-<h1>
-Setup Database (or update).</h1>
-
+    <html>
+        <head>
+            <title>
+                Setup Database
+            </title>
+        </head>
+        
+        <body>
+            
+            <h1 class="">
+                Setup Database (or update).
+            </h1>
+            
+            <div class="myForm">
+                <form action="run_setup.php" method="post">
+                    Please give the server name, usually "localhost." <br>
+                    <input type="text" name="servername"><br>
+                    Please give your username on phpmyadmin. If you haven't changed it, it's "root".<br>
+                    <input type="text" name="username"><br>
+                    Please give your password on phpmyadmin. If you haven't changed it, put in "0".<br>
+                    <input type="text" name="password"><br>
+                    <button type="submit"></button>
+                </form>
+            </div>
+        </body>
+    </html>
