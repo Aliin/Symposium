130edc40d415be954bbf8f9858c8a419e55f1832
Jesper Hodge <Aliin@users.noreply.github.com>
2016-06-06 14:17:51 +0200

    Update setup_functions.php
    added some sample data for the database.

--- database/setup_functions.php
+++ database/setup_functions.php
@@ -39,7 +39,6 @@ function setup_db() {
 	  DROP TABLE posts; IF OBJECT_ID('people', 'U') IS NOT NULL 
 	  DROP TABLE people; IF OBJECT_ID('categories', 'U') IS NOT NULL 
 	  DROP TABLE categories";
-  
   	mysqli_query($connection, $clearTables);
 	
 	//creates the post table which holds all the data a forum post has.
@@ -51,7 +50,6 @@ function setup_db() {
 	 category_id INT NOT NULL,
 	 reg_date TIMESTAMP
 	 )";
-	 
 	mysqli_query($connection, $sql);
 	 
 	//the people table holds user information, for example for post authors.
@@ -64,7 +62,6 @@ function setup_db() {
 	 email VARCHAR(50) NOT NULL,
 	 url VARCHAR(155)
 	 )";
-	 
 	mysqli_query($connection, $sql);
 	
 	//the table holds the categories.
@@ -72,10 +69,19 @@ function setup_db() {
 	  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	  name VARCHAR(20) NOT NULL
 	  )";
-	  
-    mysqli_query($connection, $sql);
+	  mysqli_query($connection, $sql);
     
-    mysqli_close($connection);
+    //insert test values into tables
+    $sql = "INSERT INTO categories (name) VALUES ('Thriller'), ('Horror'), ('Romance');
+    		INSERT INTO people (username, first_name, last_name, password, email) VALUES 
+    			('John123', 'John', 'Smith', 'mypw', 'js@mail.com'),
+    			('Fred456', 'Fred', 'Gardner', 'passW', 'fg@mail.com');
+    		INSERT INTO posts (author_id, title, body, category_id) VALUES
+    			(0, 'James Bond', 'This is a suspenseful movie', 0),
+    			(1, 'Notting Hill', 'This is a romantic movie', 2)";
+    
+    
+	mysqli_close($connection);
     	
     return 0;
 
