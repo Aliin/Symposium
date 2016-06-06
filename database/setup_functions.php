3e0dc888524be0c84e3a7d315a4d5df40e5ba78b
Jesper Hodge <Aliin@users.noreply.github.com>
2016-06-06 14:19:43 +0200

    Update setup_functions.php

--- database/setup_functions.php
+++ database/setup_functions.php
@@ -71,16 +71,17 @@ function setup_db() {
 	  )";
 	  mysqli_query($connection, $sql);
     
-    //insert test values into tables
-    $sql = "INSERT INTO categories (name) VALUES ('Thriller'), ('Horror'), ('Romance');
-    		INSERT INTO people (username, first_name, last_name, password, email) VALUES 
-    			('John123', 'John', 'Smith', 'mypw', 'js@mail.com'),
-    			('Fred456', 'Fred', 'Gardner', 'passW', 'fg@mail.com');
-    		INSERT INTO posts (author_id, title, body, category_id) VALUES
-    			(0, 'James Bond', 'This is a suspenseful movie', 0),
-    			(1, 'Notting Hill', 'This is a romantic movie', 2)";
-    
-    
+	//insert test values into tables
+	$sql = "INSERT INTO categories (name) VALUES ('Thriller'), ('Horror'), ('Romance');
+	   		INSERT INTO people (username, first_name, last_name, password, email) VALUES 
+	    		('John123', 'John', 'Smith', 'mypw', 'js@mail.com'),
+	    		('Fred456', 'Fred', 'Gardner', 'passW', 'fg@mail.com');
+	    	INSERT INTO posts (author_id, title, body, category_id) VALUES
+	    		(0, 'James Bond', 'This is a suspenseful movie', 0),
+	    		(1, 'Notting Hill', 'This is a romantic movie', 2)";
+	mysqli_query($connection, $sql);
+	    
+	    
 	mysqli_close($connection);
     	
     return 0;
