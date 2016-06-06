6f64b85ae305212aab4b03e91dbd758dcf75f231
Jesper Hodge <Aliin@users.noreply.github.com>
2016-06-06 17:03:04 +0200

    Update connectDB.php

--- includes/connectDB.php
+++ includes/connectDB.php
@@ -1,5 +1,12 @@
 <?PHP
 
 $servername = "localhost";
+$username = "symposium_server";
+$password = "my_symposium";
+$database = "symposium";
 
-$connection = 
+$connection = new mysqli($servername, $username, $password, $database);
+
+if ($connection->connect_error) {
+    die('Connect error' . $connection->connect_error);
+}
