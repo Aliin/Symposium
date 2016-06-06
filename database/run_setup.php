1fc365c74159b346803cb69f4aca5a3c986476cb
Jesper Hodge <Aliin@users.noreply.github.com>
2016-06-06 11:23:51 +0200

    Update run_setup.php

--- database/run_setup.php
+++ database/run_setup.php
@@ -1,7 +1,10 @@
 <?PHP
+//This file runs a database setup and gives further instructions.
 
+//Include the file that holds the functions we need.
 require 'setup_functions.php';
 
+//run the database setup function. It should return 0 on terminating.
 $isSetup = setup_db();
 
 if ($isSetup == 0)
@@ -11,9 +14,10 @@ if ($isSetup == 0)
   echo "database couldn't be set up. Please please please make a bug report.";
 }
 
+//give further instructions.
 echo '<br>';
 echo '<h5>Next step: </h5><p class="instruction">Please go to phpmyadmin and set up access to the symposium database: <br>';
-echo 'set up a user "symposium_server" that has access to the symposium database and give him the password "my_symposium". </br>'
-echo "you can close this window now.";
+echo 'set up a user "symposium_server" that has access to the symposium database and give him the password "my_symposium". </br>';
+echo "you can close this window now. <br></p>";
 
 ?>
