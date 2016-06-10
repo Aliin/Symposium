be19c6331a5362388c9acf74cc0a9dbcbed8efe0
Patrick Black <pj@perfectimprints.com>
2016-06-10 12:24:04 -0500

    remove gitignore

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <!--
 ----------------------------------------------------------------
-@project      : Symposium Forum Software
-@version      : v.0.1.0
-@author       : Open-source
-@description  : The page that will be displayed to the user.
+@project       : Symposium Forum Software
+@version       : v0.1.0
+@author        : Open-source
+@description   : The page that will be displayed to the user.
 ----------------------------------------------------------------
 -->
 <!DOCTYPE html>
