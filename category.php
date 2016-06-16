b30b170183bf14ee131f83d1774d815344362656
Patrick Black <pj@perfectimprints.com>
2016-06-16 18:31:51 -0500

    add js and css link files

--- category.php
+++ category.php
@@ -3,9 +3,15 @@
 <head>
 	<title>Category Page</title>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/foundation.min.css">
+	<link rel="stylesheet" type="text/css" href="css/app.css">
 </head>
 <body>
 	
+	<!-- JS at the end of the page for better loadtime -->
+	<script type="text/javascript" src="js/vendor/jquery.js"></script>
+	<script type="text/javascript" src="js/vendor/foundation.min.js"></script>
+	<script type="text/javascript" src="js/app.js"></script>
 </body>
 </html>
