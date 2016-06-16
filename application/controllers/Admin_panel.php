a93512a20a48f8572744e7bd6a22d1591cca50d1
Jesper Hodge <jhodge@outlook.de>
2016-06-16 18:59:06 +0200

    Made a central admin panel
    I put the "add user" form on a secondary view. I then added a huge list
    of options to the server admin page, anything that we might need or want
    later on.

--- application/controllers/Admin_panel.php
+++ application/controllers/Admin_panel.php
@@ -5,6 +5,9 @@ class Admin_panel extends CI_Controller {
 	//for now empty constructor
 	public function __construct() {
 		parent::__construct();
+		$this->load->library('twig');
+		$this->load->helper('security');
+		$this->load->library('ion_auth');
 	}
 	
 	//This function logs you in automatically - for testing purposes only.
@@ -21,11 +24,20 @@ class Admin_panel extends CI_Controller {
 	
 	//This controller just displays the admin panel by default if the user is an admin.
 	public function index() {
-		$this->load->library('twig');
-		$this->load->helper('security');
-		$this->load->library('ion_auth');
+		
 		if ($this->ion_auth->in_group(1)) {
 			$this->twig->display('admin_panel');
+		} else {
+			$page = array('back' => "http://localhost/symposium/index.php");
+			$this->twig->display('no_rights', $page);
+			echo $page['back'];
+		}
+	}
+	
+	//this controller displays forms for the admin.
+	public function disp_forms($which_form) {
+		if ($this->ion_auth->in_group(1)) {
+			$this->twig->display($which_form);
 		} else {
 			$page = array('back' => "http://localhost/symposium/index.php");
 			$this->twig->display('no_rights', $page);
