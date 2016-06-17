c3dddcd66b5e532df678bea17b925d5d2965606f
Patrick Black <pj@perfectimprints.com>
2016-06-16 19:45:15 -0500

    add navbar

--- category.php
+++ category.php
@@ -8,10 +8,38 @@
 	<link rel="stylesheet" type="text/css" href="css/app.css">
 </head>
 <body>
-	
-	<!-- JS at the end of the page for better loadtime -->
-	<script type="text/javascript" src="js/vendor/jquery.js"></script>
-	<script type="text/javascript" src="js/vendor/foundation.min.js"></script>
-	<script type="text/javascript" src="js/app.js"></script>
+<div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
+  <button class="menu-icon" type="button" data-toggle></button>
+  <div class="title-bar-title">Menu</div>
+</div>
+
+<div class="top-bar" id="example-menu">
+  <div class="top-bar-left">
+    <ul class="dropdown menu" data-dropdown-menu>
+      <li class="menu-text">Site Title</li>
+      <li>
+        <a href="#">One</a>
+        <ul class="menu vertical">
+          <li><a href="#">One</a></li>
+          <li><a href="#">Two</a></li>
+          <li><a href="#">Three</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Two</a></li>
+      <li><a href="#">Three</a></li>
+    </ul>
+  </div>
+  <div class="top-bar-right">
+    <ul class="menu">
+      <li><input type="search" placeholder="Search"></li>
+      <li><button type="button" class="button">Search</button></li>
+    </ul>
+  </div>
+</div>	
+
+<!-- JS at the end of the page for better loadtime -->
+<script type="text/javascript" src="js/vendor/jquery.js"></script>
+<script type="text/javascript" src="js/vendor/foundation.min.js"></script>
+<script type="text/javascript" src="js/app.js"></script>
 </body>
 </html>
