674fc4ab4c36c5a972463f26d03f9d6714f80270
Patrick Black <pj@perfectimprints.com>
2016-06-16 21:52:42 -0500

    add nav

--- category.php
+++ category.php
@@ -1,45 +1,45 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Category Page</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="assets/css/foundation.min.css">
-	<link rel="stylesheet" type="text/css" href="assets/css/app.css">
+    <title>Category Page</title>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <link href="assets/css/foundation.min.css" rel="stylesheet" type=
+    "text/css">
+    <link href="assets/css/app.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-<div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
-  <button class="menu-icon" type="button" data-toggle></button>
-  <div class="title-bar-title">Menu</div>
-</div>
+    <div class="title-bar" data-hide-for="medium" data-responsive-toggle=
+    "example-menu">
+        <button class="menu-icon" data-toggle="" type="button"></button>
+        <div class="title-bar-title">
+            Menu
+        </div>
+    </div>
+    <div class="top-bar" id="example-menu">
+        <div class="top-bar-left">
+            <ul class="dropdown menu" data-dropdown-menu="">
+                <li class="menu-text">Symposium</li>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+            </ul>
+        </div>
+        <div class="top-bar-right">
+            <ul class="menu">
+                <li><input placeholder="Search" type="search"></li>
+                <li><button class="button" type="button">Search</button></li>
+            </ul>
+        </div>
+    </div>
+    
 
-<div class="top-bar" id="example-menu">
-  <div class="top-bar-left">
-    <ul class="dropdown menu" data-dropdown-menu>
-      <li class="menu-text">Site Title</li>
-      <li>
-        <a href="#">One</a>
-        <ul class="menu vertical">
-          <li><a href="#">One</a></li>
-          <li><a href="#">Two</a></li>
-          <li><a href="#">Three</a></li>
-        </ul>
-      </li>
-      <li><a href="#">Two</a></li>
-      <li><a href="#">Three</a></li>
-    </ul>
-  </div>
-  <div class="top-bar-right">
-    <ul class="menu">
-      <li><input type="search" placeholder="Search"></li>
-      <li><button type="button" class="button">Search</button></li>
-    </ul>
-  </div>
-</div>	
-
-<!-- JS at the end of the page for better loadtime -->
-<script type="text/javascript" src="assets/js/vendor/jquery.js"></script>
-<script type="text/javascript" src="assets/js/vendor/foundation.min.js"></script>
-<script type="text/javascript" src="assets/js/app.js"></script>
+    <!-- JS at the end of the page for better loadtime -->
+    <script src="assets/js/vendor/jquery.js" type="text/javascript">
+    </script> 
+    <script src="assets/js/vendor/foundation.min.js" type="text/javascript">
+    </script> 
+    <script src="assets/js/app.js" type="text/javascript">
+    </script>
 </body>
 </html>
